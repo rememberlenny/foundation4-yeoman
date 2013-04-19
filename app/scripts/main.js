7105e8f905262b1172d2a52f4813e0b3abc1b849
iham <hilbert.markus@gmail.com>
2013-04-19 12:17:47 +0200

    Moved foundation dependency to bower.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,38 +3,38 @@ require.config({
     jquery: '../components/jquery/jquery'
   },
   shim: {
-    "foundation/foundation" : { deps: ["jquery"] },
-    "foundation/foundation.alerts": { deps: ["jquery"] },
-    "foundation/foundation.clearing": { deps: ["jquery"] },
-    "foundation/foundation.cookie": { deps: ["jquery"] },
-    "foundation/foundation.dropdown": { deps: ["jquery"] },
-    "foundation/foundation.forms": { deps: ["jquery"] },
-    "foundation/foundation.joyride": { deps: ["jquery"] },
-    "foundation/foundation.magellan": { deps: ["jquery"] },
-    "foundation/foundation.orbit": { deps: ["jquery"] },
-    "foundation/foundation.placeholder": { deps: ["jquery"] },
-    "foundation/foundation.reveal": { deps: ["jquery"] },
-    "foundation/foundation.section": { deps: ["jquery"] },
-    "foundation/foundation.tooltips": { deps: ["jquery"] },
-    "foundation/foundation.topbar": { deps: ["jquery"] }
+    "../components/foundation/js/foundation/foundation" : { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.alerts": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.clearing": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.cookie": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.dropdown": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.forms": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.joyride": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.magellan": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.orbit": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.placeholder": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.reveal": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.section": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.tooltips": { deps: ["jquery"] },
+    "../components/foundation/js/foundation/foundation.topbar": { deps: ["jquery"] }
   }
 });
 
 require(["jquery",
-  "foundation/foundation",            
-  "foundation/foundation.alerts",     
-  "foundation/foundation.clearing",   
-  "foundation/foundation.cookie",    
-  "foundation/foundation.dropdown",   
-  "foundation/foundation.forms",      
-  "foundation/foundation.joyride",    
-  "foundation/foundation.magellan",   
-  "foundation/foundation.orbit",      
-  "foundation/foundation.placeholder",
-  "foundation/foundation.reveal",     
-  "foundation/foundation.section",    
-  "foundation/foundation.tooltips",   
-  "foundation/foundation.topbar"
+  "../components/foundation/js/foundation/foundation",            
+  "../components/foundation/js/foundation/foundation.alerts",     
+  "../components/foundation/js/foundation/foundation.clearing",   
+  "../components/foundation/js/foundation/foundation.cookie",    
+  "../components/foundation/js/foundation/foundation.dropdown",   
+  "../components/foundation/js/foundation/foundation.forms",      
+  "../components/foundation/js/foundation/foundation.joyride",    
+  "../components/foundation/js/foundation/foundation.magellan",   
+  "../components/foundation/js/foundation/foundation.orbit",      
+  "../components/foundation/js/foundation/foundation.placeholder",
+  "../components/foundation/js/foundation/foundation.reveal",     
+  "../components/foundation/js/foundation/foundation.section",    
+  "../components/foundation/js/foundation/foundation.tooltips",   
+  "../components/foundation/js/foundation/foundation.topbar"
 ], function ($) {
   $(document).foundation();
 });
